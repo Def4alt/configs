a7b955eafd13ce7e3b625b9902c4604535adfdf0
Andrii Olkhovych <bronzemonet@gmail.com>
2021-11-21 14:56:42 +0200

    Add various optimizations

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-------------------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTALMEM -gt 8000000 ]]; then
+sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
+
+echo "-------------------------------------------------"
+echo "Setting up mirrors for optimal download          "
+echo "-------------------------------------------------"
+pacman -S --noconfirm pacman-contrib curl
+pacman -S --noconfirm reflector rsync
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+#Add parallel downloading
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+
+#Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+pacman -Sy --noconfirm
 
 PKGS=(
 	'steam'
@@ -19,7 +44,6 @@ PKGS=(
 	'base-devel'
 )
 
-pacman-mirrors --fasttrack 5 && pacman -Syyu
 pacman -S --noconfirm --needed ${PKGS[@]}
 
 PKGS_TO_REMOVE=(
