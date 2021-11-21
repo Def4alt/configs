6e20cfa6d966f16a98d6dda88e03a9790e6bc637
Andrii Olkhovych <bronzemonet@gmail.com>
2021-11-21 14:26:17 +0200

    Add yakuake to remove

--- scripts/system.sh
+++ scripts/system.sh
@@ -14,10 +14,19 @@ PKGS=(
 	'neovim'
 	'telegram-desktop'
 	'discord'
+	'htop'
 )
 
 pacman-mirrors --fasttrack 5 && pacman -Syyu
 pacman -S --noconfirm --needed ${PKGS[@]}
+
+PKGS_TO_REMOVE=(
+	'qt5-tools'
+	'firefox'
+	'yakuake'
+)
+
+pacman -Rs --noconfirm ${PKGS_TO_REMOVE[@]}
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
